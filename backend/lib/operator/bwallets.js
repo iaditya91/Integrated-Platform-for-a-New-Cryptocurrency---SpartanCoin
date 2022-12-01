68cb4b03a888f6898f4ba2474e11854be5358870
Aditya <iaditya91@gmail.com>
2022-11-30 18:54:28 -0800

    Add files via upload

---
+++ backend/lib/operator/bwallets.js
@@ -0,0 +1,12 @@
+const Wallet = require('./bwallet');
+const R = require('ramda');
+
+class Wallets extends Array {
+    static fromJson(data) {
+        let wallets = new Wallets();
+        R.forEach((wallet) => { wallets.push(Wallet.fromJson(wallet)); }, data);
+        return wallets;
+    }
+}
+
+module.exports = Wallets;
